c7f23229e3c2d1b0b1b2e4515f7db79a1bc86812
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-07-17 07:03:42 +0000

    Updated conditions and changed emojis

--- main.rb
+++ main.rb
@@ -16,45 +16,45 @@ end
 
 def do_math
   @turn +=1
-  if @turn %2 != 0 && @player1.is_alive? && @player2.is_alive?
+  if @turn.odd? && @player1.is_alive? && @player2.is_alive?
     if @turn != 1
-      puts"--- 🆕 TURN 🎲  ---"
+      puts "--- 🆕 TURN ---"
     end
     question = Questions.new # instance of new question
-    puts " Player 1: #{question.show}"
+    puts "Player 1: #{question.show}"
     print "> " # new line
     answer = gets.chomp.to_i
     if answer == question.answer
-      puts "Player 1: 👊 Yas! You are 💯% right!"
-      puts "P1: #{@player1.lives} / 𝟑  vs P2: #{@player2.lives} / 𝟑  "
+      puts "Player 1: 👊 Yas!You are 💯% right!"
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
     else 
       @player1.lives -= 1 # lose a life if mis-answer a question
       puts "Player 1: Seriously!? 👻  No!"
-      puts "P1: #{@player1.lives} / 𝟑 vs P2: #{@player2.lives} / 𝟑  "
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
     end
     do_math
-  elsif @turn %2 == 0 && @player1.is_alive? && @player2.is_alive?
-    puts"--- 🆕  TURN 🎲  ---"
+  elsif @turn.even? && @player1.is_alive? && @player2.is_alive?
+    puts"--- 🆕 TURN ---"
     question = Questions.new 
-    puts " Player 2: #{question.show}"
+    puts "Player 2: #{question.show}"
     print "> "
     answer = gets.chomp.to_i
     if answer == question.answer
-      puts "Player 2: 👊 Yas! You are 💯% right!"
-      puts "P1: #{@player1.lives} / 𝟑 vs P2: #{@player2.lives} / 𝟑 "
+      puts "Player 2: 👊 Yas!You are 💯% right!"
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
     else 
       @player2.lives -= 1
       puts "Player 2: Seriously!? 👻  No!"
-      puts "P1: #{@player1.lives} / 𝟑 vs P2: #{@player2.lives} / 𝟑 "
+      puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
     end
     do_math
   else
     if !(@player1.is_alive?)
-      puts "Player 2 is 👑  with a final score of #{@player2.lives} / 𝟑 "
+      puts "Player 2 is 👑  with a final score of #{@player2.lives}/3"
       puts "🔥 GAME 👀 V 🤗 R 🔥 "
       puts "👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 "
     else
-      puts "Player 1 is 👑  with a final score of #{@player1.lives} / 𝟑 "
+      puts "Player 1 is 👑  with a final score of #{@player1.lives}/3"
       puts "🔥 GAME 👀 V 🤗 R 🔥 "
       puts "👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 👋 " 
     end
@@ -62,6 +62,8 @@ def do_math
 end
 
 start_game
+
+
       
 
 
